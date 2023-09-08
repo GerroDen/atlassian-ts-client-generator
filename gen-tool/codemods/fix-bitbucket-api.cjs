866dec48adf5414b151ae598c55975196dc45367
Gerro Den <GerroDen@users.noreply.github.com>
2023-09-08 17:12:39 +0200

    Handle optional file path in codemods

--- gen-tool/codemods/fix-bitbucket-api.cjs
+++ gen-tool/codemods/fix-bitbucket-api.cjs
@@ -3,29 +3,29 @@ const transformer = (file, { j }) => {
   const source = j(file.source);
   source
     .find(j.ClassMethod)
-    .filter((path) => path.value.key?.name === "streamRaw")
+    .filter((path) => path.node.key?.name === "streamRaw")
     .filter(
       (path) =>
-        path.value.body?.body[0]?.expression?.argument?.callee?.property
+        path.node.body?.body[0]?.expression?.argument?.callee?.property
           ?.name !== "streamRawRaw",
     )
     .forEach((path) => {
-      path.value.key.name = "_streamRaw";
+      path.node.key.name = "_streamRaw";
     });
   source
     .find(j.CallExpression)
-    .filter((path) => path.value.callee?.type === "MemberExpression")
-    .filter((path) => path.value.callee?.property?.name === "streamRaw")
+    .filter((path) => path.node.callee?.type === "MemberExpression")
+    .filter((path) => path.node.callee?.property?.name === "streamRaw")
     .forEach((path) => {
-      path.value.callee.property.name = "_streamRaw";
+      path.node.callee.property.name = "_streamRaw";
     });
-  if (!file.path.endsWith("/models/index.ts")) {
+  if (!file.path?.endsWith("/models/index.ts")) {
     source
       .find(j.ExportNamedDeclaration)
       .filter(
-        (path) => path.value.declaration?.type === "TSInterfaceDeclaration",
+        (path) => path.node.declaration?.type === "TSInterfaceDeclaration",
       )
-      .filter((path) => path.value.declaration?.id?.name?.endsWith("Request"))
+      .filter((path) => path.node.declaration?.id?.name?.endsWith("Request"))
       .replaceWith((path) => path.node.declaration);
   }
   return source.toSource();
